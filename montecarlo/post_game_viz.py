8d678a4286faa64e600f787cb9e297a2eb321963
Enzo Villafuerte <ev693020@ohio.edu>
2024-12-25 10:11:44 -0500

    Update post_game_viz.py

--- montecarlo/post_game_viz.py
+++ montecarlo/post_game_viz.py
@@ -624,4 +624,4 @@ Further improvements:
 # python montecarlo/post_game_viz.py 'https://understat.com/match/26726'
 # python montecarlo/post_game_viz.py 'https://understat.com/match/27864'
 # python montecarlo/post_game_viz.py 'https://understat.com/match/26753'
-# python montecarlo/post_game_viz.py 'https://understat.com/match/27143'
+# python montecarlo/post_game_viz.py 'https://understat.com/match/27160'
