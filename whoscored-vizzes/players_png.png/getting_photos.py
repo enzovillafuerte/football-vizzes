d07870e33b71e570da3622bf6d74f4cd9a52f30f
Enzo Villafuerte <ev693020@ohio.edu>
2025-02-07 17:37:49 -0500

    Update getting_photos.py

--- whoscored-vizzes/players_png.png/getting_photos.py
+++ whoscored-vizzes/players_png.png/getting_photos.py
@@ -5,7 +5,7 @@ player_dict = {
     'Aldo Corzo': 118213,
     'William Riveros': 807783,
     'Di Benedetto': 725300,
-    'Dulanto': ,
+    'Dulanto': 661777,
     'R. Guzmán': ,
     'P. Reyna': 1028040,
     'C. Inga': 1415328,
@@ -26,13 +26,78 @@ player_dict = {
 
 
     # Sporting Cristal
+    # https://www.clubsportingcristal.pe/index.php/plantel-profesional
+    'D. Enríquez': ,
+    'R. Solís': ,
+    'A. Duarte': ,
+    'C. Bautista': ,
+    'F. Alcedo': ,
+    'G. Chávez': ,
+    'R. Lutiger': ,
+    'L. Sosa': ,
+    'J. Lora': ,
+    'F. Romero': ,
+    'N. Pasquini': ,
+    'A. Pósito': ,
+    'L. Díaz': ,
+    'F. Cassiano': ,
+    'F. Lora': ,
+    'J. Pretell': ,
+    'C. Gonzales': ,
+    'Y. Yotún': ,
+    'Maxloren Castro': ,
+    'M. Távara': ,
+    'Ian Wisdom': ,
+    'Jostin Alarcón': 1254284,
+    'Y. Del Valle': ,
+    'S. Sánchez': ,
+    'A. Beltrán': ,
+    'G. Cazonatti': 872394,
+    'Catriel Cabellos': 1418741,
+    'S. González': 892828,
+    'Cauteruccio': 115871,
+    'I. Avila': 116937,
+    'L. Iberico': 547724,
+    'M. Sosa': ,
+    'F. Pacheco': 844386 ,
+
+
 
 
     # Alianza Lima
     # https://clubalianzalima.com.pe/#/plantilla/ficha/Ricardo-Lagos
 
+    'G. Vizcarra': 493691,
+    'J. Castillo': 964199,
+    'R. Garcés': 751527,
+    'M. Huamán': 1248844,
+    'Ricardo Lagos': 1086550,
+    "Jhamir D'Arrigo": 1181405,
+    # 'De La Cruz': 
+    'Eryc Castillo': 458861,
+    'A. Campos': 427217,
+    'M. Succar': 982363,
+    'Archimbaud': 547854,
+    'Gaibor': 200257,
+    'Trauco': 248704,
+    'Lavandeira': 127268,
+    'R. Alarcón': ,
+    'J. Velásquez': ,
+    'Ceppelini': 166121,
+    'H. Barcos': 18694,
+    'C. Gómez': ,
+    'A. Cantero': 1098460,
+    'J. Delgado': ,
+    'G. Enrique': 1312034,
+    'S. Peralta': ,
+    'J. Navea': ,
+    'K. Quevedo': 831684,
 
     # Melgar 
+    # https://espndeportes.espn.com/futbol/equipo/plantel/_/id/7312/team.melgar
+    'Carlos Cáceda': ,
+    'R. Farro': ,
+    'P. Barrios'
 
 
 
