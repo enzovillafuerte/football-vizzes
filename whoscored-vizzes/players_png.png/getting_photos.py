d7447c3e9841369f5d1679c492ffcd3def7d1af5
Enzo Villafuerte <ev693020@ohio.edu>
2025-02-06 21:08:11 -0500

    Image urls

---
+++ whoscored-vizzes/players_png.png/getting_photos.py
@@ -0,0 +1,49 @@
+player_dict = {
+
+    # Universitario
+    'S. Britos' : 343431, 
+    'Aldo Corzo': 118213,
+    'William Riveros': 807783,
+    'Di Benedetto': 725300,
+    'Dulanto': ,
+    'R. Guzmán': ,
+    'P. Reyna': 1028040,
+    'C. Inga': 1415328,
+    'Andy Polo': 263619,
+    'Ancajima': 1432790,
+    'R. Ureña': 304875,
+    'Murrugarra': 921734,
+    'Calcaterra': 146810,
+    'J. Concha': 988208,
+    'Perez Guedes': 280141,
+    'J. Velez': 526949,
+    'Gabriel Costa': 389888,
+    'E. Flores': 315926,
+    'J. Rivera': 847129,
+    'D. Churín': 113608,
+    'A. Valera': 1133998,
+    'Carabalí': 1130861,
+
+
+    # Sporting Cristal
+
+
+    # Alianza Lima
+    # https://clubalianzalima.com.pe/#/plantilla/ficha/Ricardo-Lagos
+
+
+    # Melgar 
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
